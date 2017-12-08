cf905dd0b52d18476b572121c65c698ec15a3e52
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2017-12-08 15:41:25 -0500

    first commit

---
+++ anscombe.R
@@ -0,0 +1,20 @@
+library(datasets)
+library(readr)
+library(data.table)
+
+df=anscombe
+df$id <- 1:nrow(df)
+
+long <-  melt(as.data.table(df), id.vars = "id", 
+              measure.vars = patterns("x", "y"),
+              value.name = c("x", "y"),
+              variable.name = "dataset")
+
+library(ggplot2)
+ggplot(long, aes(x = x, y = y)) + 
+  geom_point() +
+  facet_wrap(~dataset, ncol = 2, labeller = label_both)
+ggsave("anscombe_scatter_ggplot.pdf", width = 5, heigh = 3)
+
+write_csv(long, "anscombe_long.csv")
+write_csv(df, "anscombe_wide.csv")
